341d3fc6d919ed9b40c00a25a92a25b5b5596ee4
sachindu kavishka <cst21017@std.uwu.ac.lk>
2023-11-16 23:53:01 +0530

    add images

--- lib/Game.dart
+++ lib/Game.dart
@@ -15,10 +15,34 @@ class _GameState extends State<Game> {
   _GameState(this.player1, this.player2);
   bool player01Turn = true;
 
+
+  List imageName = [];
+  // Initializing image list with empty list items
+  @override
+  void initState() {
+    super.initState();
+    // TODO: implement initState
+    for(int i = 0; i < 9; i++) {
+      imageName.add("blank.png");
+    }
+  }
+
+  void clickOnSq(int num) {
+    setState(() {
+      if(player01Turn) {
+        imageName[num] = 'cancel.png';
+      } else {
+        imageName[num] = 'o.png';
+      }
+    });
+    player01Turn = !player01Turn;
+  }
+
   @override
   Widget build(BuildContext context) {
     print('$player1  $player2');
     double screenWidth = MediaQuery.of(context).size.width;
+    double miniCon = (screenWidth-85)/3;
     return Scaffold(
       backgroundColor: const Color(0xFF0E2127),
       body: Center(
@@ -50,6 +74,185 @@ class _GameState extends State<Game> {
                 decoration: BoxDecoration(
                   color: const Color(0xFF584D4D),
                   border: Border.all(color: Colors.white, width: 4)
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8, bottom: 4),
+                      child: Row(
+                        children: [
+
+                          InkWell(
+                            onTap: () {
+                              clickOnSq(0);
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.only(left: 8, right: 4),
+                              padding: const EdgeInsets.all(10),
+                              width: miniCon,
+                              height: miniCon,
+                              decoration: BoxDecoration(
+                                color: const Color(0xFF231D1D),
+                                image: DecorationImage(image: AssetImage('assets/images/${imageName[0]}'),
+                                    fit: BoxFit.cover)
+                              ),
+                            ),
+                          ),
+
+                          InkWell(
+                            onTap: () {
+                              clickOnSq(1);
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.only(left: 8, right: 4),
+                              padding: const EdgeInsets.all(10),
+                              width: miniCon,
+                              height: miniCon,
+                              decoration: BoxDecoration(
+                                  color: const Color(0xFF231D1D),
+                                  image: DecorationImage(image: AssetImage('assets/images/${imageName[1]}'),
+                                      fit: BoxFit.cover)
+                              ),
+                            ),
+                          ),
+
+                          InkWell(
+                            onTap: () {
+                              clickOnSq(2);
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.only(left: 8, right: 4),
+                              padding: const EdgeInsets.all(10),
+                              width: miniCon,
+                              height: miniCon,
+                              decoration: BoxDecoration(
+                                  color: const Color(0xFF231D1D),
+                                  image: DecorationImage(image: AssetImage('assets/images/${imageName[2]}'),
+                                      fit: BoxFit.cover)
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 4),
+                      child: Row(
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              clickOnSq(3);
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.only(left: 8, right: 4),
+                              padding: const EdgeInsets.all(10),
+                              width: miniCon,
+                              height: miniCon,
+                              decoration: BoxDecoration(
+                                  color: const Color(0xFF231D1D),
+                                  image: DecorationImage(image: AssetImage('assets/images/${imageName[3]}'),
+                                      fit: BoxFit.cover)
+                              ),
+                            ),
+                          ),
+
+                          InkWell(
+                            onTap: () {
+                              clickOnSq(4);
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.only(left: 8, right: 4),
+                              padding: const EdgeInsets.all(10),
+                              width: miniCon,
+                              height: miniCon,
+                              decoration: BoxDecoration(
+                                  color: const Color(0xFF231D1D),
+                                  image: DecorationImage(image: AssetImage('assets/images/${imageName[4]}'),
+                                      fit: BoxFit.cover)
+                              ),
+                            ),
+                          ),
+
+                          InkWell(
+                            onTap: () {
+                              clickOnSq(5);
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.only(left: 8, right: 4),
+                              padding: const EdgeInsets.all(10),
+                              width: miniCon,
+                              height: miniCon,
+                              decoration: BoxDecoration(
+                                  color: const Color(0xFF231D1D),
+                                  image: DecorationImage(image: AssetImage('assets/images/${imageName[5]}'),
+                                      fit: BoxFit.cover)
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.only(top: 4, bottom: 8),
+                      child: Row(
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              clickOnSq(6);
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.only(left: 8, right: 4),
+                              padding: const EdgeInsets.all(10),
+                              width: miniCon,
+                              height: miniCon,
+                              decoration: BoxDecoration(
+                                  color: const Color(0xFF231D1D),
+                                  image: DecorationImage(image: AssetImage('assets/images/${imageName[6]}'),
+                                      fit: BoxFit.cover)
+                              ),
+                            ),
+                          ),
+
+                          InkWell(
+                            onTap: () {
+                              clickOnSq(7);
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.only(left: 8, right: 4),
+                              padding: const EdgeInsets.all(10),
+                              width: miniCon,
+                              height: miniCon,
+                              decoration: BoxDecoration(
+                                  color: const Color(0xFF231D1D),
+                                  image: DecorationImage(image: AssetImage('assets/images/${imageName[7]}'),
+                                      fit: BoxFit.cover)
+                              ),
+                            ),
+                          ),
+
+                          InkWell(
+                            onTap: () {
+                              clickOnSq(8);
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.only(left: 8, right: 4),
+                              padding: const EdgeInsets.all(10),
+                              width: miniCon,
+                              height: miniCon,
+                              decoration: BoxDecoration(
+                                  color: const Color(0xFF231D1D),
+                                  image: DecorationImage(image: AssetImage('assets/images/${imageName[8]}'),
+                                      fit: BoxFit.cover)
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+
+                  ],
                 ),
               ),
             ),
